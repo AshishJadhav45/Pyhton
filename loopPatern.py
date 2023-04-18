e30dc111a2bf736d9d7b1dab474dfb54df878bc1
AshishJadhav <vinodjadhav1845@gmail.com>
2023-04-18 20:20:29 +0530

    loop patern program

--- loopPatern.py
+++ loopPatern.py
@@ -13,9 +13,9 @@ for i in range(1, row + 1, 1):
     # empty line after each row
     print("")
 
-# n = 5
-# k = 5
-# for i in range(0,n+1):
-#     for j in range(k-i,0,-1):
-#         print(j,end=' ')
-#     print()
+n = 5
+k = 5
+for i in range(0,n+1):
+    for j in range(k-i,0,-1):
+        print(j,end=' ')
+    print()
