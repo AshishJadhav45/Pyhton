4266a29922054295bf45aa0294ef463c7ec60f0e
JADHAV ASHISH <77589461+AshishJadhav45@users.noreply.github.com>
2023-05-02 22:07:58 +0530

    Add files via upload

--- function.py
+++ function.py
@@ -1,13 +1,17 @@
-def nameAge(name, age):
-	print("Hi, I am", name)
-	print("My age is ", age)
+# edit the functions prototype and implementation
+def foo(a, b, c, *args):
+    return len(args)
+
+def bar(a, b, c, **kwargs):
+    return kwargs["magicnumber"] == 7
 
 
-# You will get correct output because
-# argument is given in order
-print("Case-1:")
-nameAge("Suraj", 27)
-# You will get incorrect output because
-# argument is not in order
-print("\nCase-2:")
-nameAge(27, "Suraj")
+# test code
+if foo(1, 2, 3, 4) == 1:
+    print("Good.")
+if foo(1, 2, 3, 4, 5) == 2:
+    print("Better.")
+if bar(1, 2, 3, magicnumber=6) == False:
+    print("Great.")
+if bar(1, 2, 3, magicnumber=7) == True:
+    print("Awesome!")
