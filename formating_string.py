171f3c22be05c939452807fe24e01343e2015479
AshishJadhav <vinodjadhav1845@gmail.com>
2023-06-15 23:34:33 +0530

    formating_string

---
+++ formating_string.py
@@ -0,0 +1,12 @@
+name = "butcher"
+number = len(name) * 2
+print("hello {} , your lucky number is {}".format(name,number))
+
+
+first = 'apple'
+second = "banana"
+third = "Carrot"
+
+formatted_string = "{0} {2} {1}".format(first,second,third)
+
+print(formatted_string)
