bf96502b70291664390411a3b98e5f36049e6c7e
AshishJadhav <vinodjadhav1845@gmail.com>
2023-06-18 20:20:20 +0530

    lsit email program

---
+++ map_function.py
@@ -0,0 +1,11 @@
+# Map function Program 
+
+# map(function,iterable)
+# function = lambda x: x*2
+# iterable = number
+
+number = [1,3,4,6,7,34]
+
+double = map(lambda x: x*2,number)
+
+print(list(double))
---
+++ static_methos.py
@@ -0,0 +1,23 @@
+class math:
+    def __int__(self, num ):
+        self.num = num 
+
+def addtoNum(self,n):
+    self.num = self.num + n
+
+@staticmethod
+def add(a,b):
+    return a+b
+
+
+# result = math.add(5,4)
+# print(result)
+
+
+# a = math(5)
+print(a.num)
+
+a.addtoNum(5)
+print(a.num)
+
+print(math.add(5,4))
