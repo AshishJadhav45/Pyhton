251e08181c6aa54fc7d37362ab488c243b41bf02
AshishJadhav <vinodjadhav1845@gmail.com>
2023-07-05 17:31:06 +0530

    changing string program

---
+++ change_string.py
@@ -0,0 +1,16 @@
+
+# change string 
+
+def change_string(given_string):
+    new_string = ""
+
+    new_list = given_string.split()
+
+    for element in new_list:
+        new_string += element[1:] + "_" + element[0] + " "
+
+    return new_string
+
+print(change_string("1one 2two 3three 4four 5five "))
+
+# Should print "one-1 two-2 three-3 four-4 five-5"  
