cde503eb92f752eb4de1bcb7dd068ffdf01f4dfe
AshishJadhav <vinodjadhav1845@gmail.com>
2023-07-13 23:58:36 +0530

    Geometric mean program

--- geometric_mean.py
+++ geometric_mean.py
@@ -1,5 +1,38 @@
+# Normal method
 a = 10
 b = 6
 
 c = (a*b)/(a+b)
 print(c)
+
+
+# Using Function
+
+def geometric_mean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if (a>b):
+        print("first number is greater than second number")
+    else:
+        print("second number is greater than first number")
+
+def isLesser(a,b):
+    pass
+    
+
+a = 10
+b = 9
+
+geometric_mean(a,b)
+
+isGreater(a,b)
+
+
+c = 10
+d = 20
+
+geometric_mean(c,d)
+isGreater(c,d)
+
