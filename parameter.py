a151b655d406f3b727bdbf3ac8de37ae368d6f99
AshishJadhav <vinodjadhav1845@gmail.com>
2023-07-16 20:29:29 +0530

    parmeter program

---
+++ parameter.py
@@ -0,0 +1,5 @@
+def increment(a, b=1):
+# 'a' is the value and 'b' is the value by which 'a' will be
+increased return a+b
+print("Value when all parameters are provided",increment(4, 3))
+print("Value when all parameters are not provided",increment(4))
