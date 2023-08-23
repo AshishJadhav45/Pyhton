ca1b565db3bfb65bedc7f32cad42ec21ecc14850
AshishJadhav <vinodjadhav1845@gmail.com>
2023-08-23 23:24:56 +0530

    count program using for loop

---
+++ count.py
@@ -0,0 +1,8 @@
+# count the all list numbers and find out the total sum
+
+count = 0
+
+for num in [1,2,3,4,5,6,7,8,9]:
+    count = count+num
+    print(num,count)
+print("total sum of number is ",count)
