7a72c38e432eaf59c24cd010196662b579ea01bf
AshishJadhav <vinodjadhav1845@gmail.com>
2023-08-23 23:27:05 +0530

    find out largest number from the list using for loop

---
+++ largest.py
@@ -0,0 +1,12 @@
+# find out the largest number from the list 
+
+# first we initialize smallest number for comparassion 
+
+first_number = 0
+
+for numbers in [11,33,45,32,11,55,332,567,45]:
+    if numbers > first_number:
+        first_number = numbers
+    print(numbers,first_number)
+
+print("the largest number is ",first_number)
